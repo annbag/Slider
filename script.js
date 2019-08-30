b8f8c68ba9b4791ee94b55f657a6b82691a423b5
Anna Ambroziak <ambroziak_anna@wp.pl>
2019-08-30 10:08:16 +0200

    get data from data.json

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $.getJSON('https://annbag.github.io/Slider/data.json', function (data) {
-    let output = "<ul>";
     console.log(data);
-    for (let i in data) {
-        output += "<li>" + data[i].id + "</li>";
-        output += "<li>" + data[i].title + "</li>";
-        output += "<img src=" + data[i].imageUrl + ">";
-    }
+    let output = "<ul>";
+    data.forEach(image => {
+        output += "<li>";
+        output += `<img src="${image.imageUrl}" >`;
+        output += "</li>";
+    });
+
     output += "</ul>";
     document.getElementById('images').innerHTML = output;
 });
