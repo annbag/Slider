23f4f8454454673631bea30b89db2dcef6227f66
Anna Ambroziak <ambroziak_anna@wp.pl>
2019-10-02 15:01:58 +0200

    create class Slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,113 +1,114 @@
-const sliderButtonNext = document.querySelector('.right-arrow');
-const sliderButtonPrev = document.querySelector('.left-arrow');
-const sliderElements = document.getElementsByTagName('li');
-const sliderDotsElement = document.getElementsByClassName('slider-dot-btn');
+class Slider {
+    constructor(elemSelector) {
+        this.sliderSelector = elemSelector;
+        this.sliderButtonNext = null;
+        this.sliderButtonPrev = null;
+        this.sliderElements = null;
+        this.sliderDotsElement = null;
 
-const time = 3000;
-let activeElement = 0;
-let interval;
+        this.activeElement = 0;
+        this.time = 3000;
+        this.slider = null;
+        this.interval = null;
 
-function main() {
+        this.generateSlider();
+        this.main();
+    }
+}
+
+Slider.prototype.generateSlider = function () {
+    this.slider = document.querySelector(this.sliderSelector);
+    this.sliderButtonNext = document.querySelector('.right-arrow');
+    this.sliderButtonPrev = document.querySelector('.left-arrow');
+    this.sliderElements = document.getElementsByTagName('li');
+    this.sliderDotsElement = document.getElementsByClassName('slider-dot-btn');
+};
+
+Slider.prototype.main = function () {
     fetchImages()
         .then((images) => {
             renderImages(images);
-            interval = setInterval(nextImage, time);
+            // this.interval = setInterval(this.nextImage, this.time);
+            this.interval = setTimeout(function () {
+                this.nextImage();
+            }.bind(this), this.time);
 
-            sliderButtonNext.addEventListener('click', () => {
-                nextImage();
-                clearInterval(interval);
+            this.sliderButtonNext.addEventListener('click', () => {
+                this.nextImage();
+                clearInterval(this.interval);
             });
-            sliderButtonPrev.addEventListener('click', () => {
-                prevImage();
-                clearInterval(interval);
+            this.sliderButtonPrev.addEventListener('click', () => {
+                this.prevImage();
+                clearInterval(this.interval);
             });
 
-            // addDots();
-            createDots()
-            addActiveClassToFirstElement();
+            this.createDots();
+            this.addActiveClassToFirstElement();
         });
-}
+};
 
-function nextImage() {
-    activeElement++;
-
-    const isActiveElementOutOfElementCollection = (activeElement === sliderElements.length);
-    const numberPhotos = sliderElements.length - 1;
+Slider.prototype.nextImage = function () {
+    this.activeElement++;
+    const isActiveElementOutOfElementCollection = (this.activeElement >= this.sliderElements.length);
 
     if (isActiveElementOutOfElementCollection) {
-        sliderElements[0].classList.add('active');
-        sliderElements[numberPhotos].classList.remove('active');
-
-        sliderDotsElement[0].classList.add('active');
-        sliderDotsElement[numberPhotos].classList.remove('active');
-
-        activeElement = 0;
-    } else {
-        sliderElements[activeElement].classList.add('active');
-        sliderElements[activeElement - 1].classList.remove('active');
-
-        sliderDotsElement[activeElement].classList.add('active');
-        sliderDotsElement[activeElement - 1].classList.remove('active');
+        this.activeElement = 0;
     }
-}
 
-function prevImage() {
-    activeElement--;
+    this.changeSlide(this.activeElement);
+};
 
-    const isActiveElementBelowZero = (activeElement === -1);
+Slider.prototype.prevImage = function () {
+    this.activeElement--;
 
-    if (isActiveElementBelowZero) {
-        activeElement = sliderElements.length - 1;
-        sliderElements[0].classList.remove('active');
+    const isActiveElementBelowZero = (this.activeElement < 0);
 
-        sliderDotsElement[0].classList.remove('active');
-    } else {
-        sliderElements[activeElement].classList.add('active');
-        sliderElements[activeElement + 1].classList.remove('active');
-
-        sliderDotsElement[activeElement].classList.add('active');
-        sliderDotsElement[activeElement + 1].classList.remove('active');
+    if (this.activeElement < isActiveElementBelowZero) {
+        this.activeElement = this.sliderElements.length - 1;
     }
-}
 
-function addActiveClassToFirstElement() {
+    this.changeSlide(this.activeElement);
+};
+
+Slider.prototype.addActiveClassToFirstElement = function () {
     const firstPhoto = document.querySelector('.slider-element');
     firstPhoto.classList.add('active');
 
     const firstDot = document.querySelector('.slider-dot-btn');
     firstDot.classList.add('active');
-}
+};
 
-function createDots() {
+Slider.prototype.createDots = function () {
     const sliderDots = document.createElement('div');
     sliderDots.classList.add('slider-dots');
-    for (let i = 0; i < sliderElements.length; i++) {
 
+    for (let i = 0; i < this.sliderElements.length; i++) {
         const dotBtn = document.createElement('button');
         dotBtn.classList.add('slider-dot-btn');
         dotBtn.type = 'button';
 
-        dotBtn.addEventListener('click', function() {
-            changeSlide(i);
-        });
+        dotBtn.addEventListener('click', function () {
+            this.changeSlide(i);
+        }.bind(this));
 
         sliderDots.appendChild(dotBtn);
     }
 
     document.querySelector('.slider').appendChild(sliderDots);
-}
+};
 
-function changeSlide(index) {
-    for (let i = 0; i < sliderElements.length; i++) {
-        sliderElements[i].classList.remove('active');
-        sliderDotsElement[i].classList.remove('active');
-        clearInterval(interval);
+Slider.prototype.changeSlide = function (index) {
+    for (let i = 0; i < this.sliderElements.length; i++) {
+        this.sliderElements[i].classList.remove('active');
+        this.sliderDotsElement[i].classList.remove('active');
+        clearInterval(this.interval);
     }
+    this.activeElement = index;
 
-    sliderElements[index].classList.add('active');
-    sliderDotsElement[index].classList.add('active');
+    this.sliderElements[this.activeElement].classList.add('active');
+    this.sliderDotsElement[this.activeElement].classList.add('active');
+};
 
-    activeElement = index;
-}
+const slide1 = new Slider('#slider1');
 
-window.addEventListener('DOMContentLoaded', main);
+// window.addEventListener('DOMContentLoaded', Slider.prototype.main);
