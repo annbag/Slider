dabcffa33df07db7146e8bf0f2951e83b25c6fac
Anna Ambroziak <ambroziak_anna@wp.pl>
2019-10-23 19:49:49 +0200

    remove ul form html structure and create ul by js

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,12 +1,19 @@
 // Process: Render
 function renderImages(images) {
+    const sliderSelector = document.querySelector('.slider');
+
+    const sliderElements = document.createElement('ul');
+    sliderElements.classList.add('slider-elements');
+    sliderSelector.appendChild(sliderElements);
 
     images.forEach(image => {
         const sliderElement = document.createElement('li');
         sliderElement.classList.add('slider-element');
         const sliderImage = document.createElement('img');
-        document.querySelector('.slider-elements').appendChild(sliderElement);
+
+        sliderElements.appendChild(sliderElement);
         sliderElement.appendChild(sliderImage);
         sliderImage.src = image.imageUrl;
     });
+
 }
